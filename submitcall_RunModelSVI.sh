cc9833a925b370e7a8bfd1a4f48cd2aa6bfb0290
ecox10 <49499833+ecox10@users.noreply.github.com>
2022-06-29 13:06:50 -0600

    Update submitcall_RunModelSVI.sh

--- submitcall_RunModelSVI.sh
+++ submitcall_RunModelSVI.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-
-### Sets the job name
+### sets the job’s name
 #SBATCH --job-name=RunSVIModel
-###Sets the job output file and path
-# SBATCH --output=RunSVIModel.out. %j
-### Sets the job error output file and path
-# SBATCH --error=RunSVIModel.err. %j
+###Sets the job’s output file and path
+#SBATCH --output=RunSVIModel.out.%j
+### Sets the job’s error output file and path
+#SBATCH --error=RunSVIModel.err.%j
 ### Requested number of nodes for this job. Can be a single number or a range
-# -N 4
+#SBATCH -N 4
 ### Requested partition (group of nodes, I.e. compute, fat, gpu, etc.) for the resource allocation
-# SBATCH -p math-alderman 
+#SBATCH -p math-alderaan
 
-singularity exec /storage/singularity/mixtures.sif /home/coxeli/stewmap/RunModelSVI.sh
+singularity exec /storage/singularity/mixtures.sif /home/coxeli/stewmap/RunModel3.sh
+
